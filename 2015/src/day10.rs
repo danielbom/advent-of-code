4df502335dda2852892a55269b9b69706bf2d14d
danielbom <daniel.rodrigues.45@hotmail.com>
2023-02-05 02:46:19 -0300

    Add day 10

---
+++ 2015/src/day10.rs
@@ -0,0 +1,57 @@
+struct GroupChars<'a> {
+    chars: &'a str,
+    index: usize,
+}
+
+impl<'a> GroupChars<'a> {
+    fn new(chars: &'a str) -> GroupChars<'a> {
+        GroupChars { chars, index: 0 }
+    }
+}
+
+impl Iterator for GroupChars<'_> {
+    type Item = (char, u32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.chars.len() {
+            None
+        } else {
+            let mut count = 1;
+            let mut index = self.index + 1;
+            let bytes = self.chars.as_bytes();
+            let byte = bytes[self.index];
+            while index < self.chars.len() && bytes[index] == byte {
+                count += 1;
+                index += 1;
+            }
+            self.index = index;
+            Some((byte as char, count))
+        }
+    }
+}
+
+fn transform(input: &str) -> String {
+    return GroupChars::new(input)
+        .map(|(c, count)| format!("{}{}", count, c))
+        .collect::<String>();
+}
+
+fn transform_times(input: &str, n: usize) -> String {
+    (0..n).fold(input.to_string(), |input, _| transform(&input))
+}
+
+fn part1(content: &str) -> usize {
+    transform_times(content, 40).len()
+}
+
+fn part2(content: &str) -> usize {
+    transform_times(content, 50).len()
+}
+
+pub fn solve() -> std::io::Result<()> {
+    let mut content = String::new();
+    aoc2015::read_file("inputs/day-10.txt", &mut content)?;
+    println!("Day 09 Part 1: {}", part1(&content));
+    println!("Day 09 Part 2: {}", part2(&content));
+    Ok(())
+}
