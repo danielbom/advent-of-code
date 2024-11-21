d957b48bf87eafcdac42eaa823f497414395fc07
danielbom <daniel.rodrigues.45@hotmail.com>
2024-11-21 18:01:26 -0300

    Add day 19

--- 2016/internal/day19/day19.go
+++ 2016/internal/day19/day19.go
@@ -3,6 +3,8 @@ package day19
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"aoc2016/internal/utils"
 )
@@ -15,20 +17,89 @@ func readAllFile(filename string) (string, error) {
 	return string(bytes), nil
 }
 
-func parseFile(filename string) (any, error) {
-	_, err := readAllFile(filename)
+func parseFile(filename string) (int, error) {
+	content, err := readAllFile(filename)
 	if err != nil {
 		return 0, err
 	}
-	return 0, err
+	content = strings.TrimSpace(content)
+	return strconv.Atoi(content)
 }
 
-func part1(input any) int {
-	return 0
+// Double Linked List Indexed
+type Elf struct {
+	Id    int
+	Prev  int
+	Next  int
+	Gifts int
 }
 
-func part2(input any) int {
-	return 0
+type Party []Elf
+
+func NewParty(elvesCount int) Party {
+	elves := make([]Elf, elvesCount)
+	for i, _ := range elves {
+		elves[i].Id = i + 1
+		elves[i].Gifts = 1
+		if i == 0 {
+			elves[i].Prev = elvesCount - 1
+		} else {
+			elves[i].Prev = i - 1
+		}
+		if i == elvesCount-1 {
+			elves[i].Next = 0
+		} else {
+			elves[i].Next = i + 1
+		}
+	}
+	return Party(elves)
+}
+
+func (party Party) Winner1() int {
+	current := 0
+	for i := 1; i < len(party); i++ {
+		{
+			stolen := party[current].Next
+			party[current].Gifts += party[stolen].Gifts
+			party[stolen].Gifts = 0
+			party[party[stolen].Prev].Next = party[stolen].Next
+			party[party[stolen].Next].Prev = party[stolen].Prev
+		}
+		current = party[current].Next
+	}
+	return party[current].Id
+}
+
+func (party Party) Winner2() int {
+	current := 0
+	count := len(party)
+	center := count / 2
+	for count > 1 {
+		{
+			stolen := center
+			party[current].Gifts += party[stolen].Gifts
+			party[stolen].Gifts = 0
+			party[party[stolen].Prev].Next = party[stolen].Next
+			party[party[stolen].Next].Prev = party[stolen].Prev
+			center = party[center].Next
+			if count%2 == 1 {
+				center = party[center].Next
+			}
+		}
+		current = party[current].Next
+		count--
+	}
+	return party[current].Id
+}
+
+func part1(elvesCount int) int {
+	party := NewParty(elvesCount)
+	return party.Winner1()
+}
+
+func part2(elvesCount int) int {
+	party := NewParty(elvesCount)
+	return party.Winner2()
 }
 
 func Solve() {
