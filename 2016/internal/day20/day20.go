b9379beedf850b29f9f934330751e5d5f0cfabab
danielbom <daniel.rodrigues.45@hotmail.com>
2024-11-22 06:45:08 -0300

    Add day 20

--- 2016/internal/day20/day20.go
+++ 2016/internal/day20/day20.go
@@ -1,8 +1,12 @@
 package day20
 
 import (
+	"cmp"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 
 	"aoc2016/internal/utils"
 )
@@ -15,20 +19,100 @@ func readAllFile(filename string) (string, error) {
 	return string(bytes), nil
 }
 
-func parseFile(filename string) (any, error) {
-	_, err := readAllFile(filename)
-	if err != nil {
-		return 0, err
+type Range struct {
+	Begin int
+	End   int
+}
+
+func NewRange(begin, end int) Range {
+	return Range{Begin: begin, End: end}
+}
+
+func ParseRange(text string) (Range, error) {
+	first, second, found := strings.Cut(text, "-")
+	if !found {
+		return NewRange(-1, -1), fmt.Errorf("missing range separator")
 	}
-	return 0, err
+	begin, err := strconv.Atoi(first)
+	if err != nil {
+		return NewRange(-1, -1), err
+	}
+	end, err := strconv.Atoi(second)
+	if err != nil {
+		return NewRange(-1, -1), err
+	}
+	return NewRange(begin, end), nil
 }
 
-func part1(input any) int {
-	return 0
+func parseContent(content string) ([]Range, error) {
+	content = strings.TrimSpace(content)
+	lines := strings.Split(content, "\n")
+	result := make([]Range, 0, len(lines))
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		r, err := ParseRange(line)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, r)
+	}
+	return result, nil
 }
 
-func part2(input any) int {
-	return 0
+func parseFile(filename string) ([]Range, error) {
+	content, err := readAllFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return parseContent(content)
+}
+
+func mergeInterleavedRanges(ranges []Range) []Range {
+	slices.SortFunc(ranges, func(r1, r2 Range) int {
+		return cmp.Compare(r1.Begin, r2.Begin)
+	})
+	result := make([]Range, 0, len(ranges))
+	result = append(result, ranges[0])
+	for _, r := range ranges {
+		if r.Begin <= result[len(result)-1].End+1 {
+			result[len(result)-1].End = utils.Max(result[len(result)-1].End, r.End)
+		} else {
+			result = append(result, r)
+		}
+	}
+	return result
+}
+
+func minimumValueAllowed(ranges []Range) int {
+	if len(ranges) == 0 {
+		return -1
+	}
+	merged := mergeInterleavedRanges(ranges)
+	return merged[0].End + 1
+}
+
+func part1(ranges []Range) int {
+	return minimumValueAllowed(ranges)
+}
+
+func allowedValuesCount(ranges []Range) int {
+	if len(ranges) == 0 {
+		return -1
+	}
+	merged := mergeInterleavedRanges(ranges)
+	count := 0
+	for i := 1; i < len(merged); i++ {
+		count += merged[i].Begin - merged[i-1].End - 1
+	}
+	maximumValue := 4294967295
+	if maximumValue > merged[len(merged)-1].End {
+		count += maximumValue - merged[len(merged)-1].End - 1
+	}
+	return count
+}
+
+func part2(ranges []Range) int {
+	return allowedValuesCount(ranges)
 }
 
 func Solve() {
